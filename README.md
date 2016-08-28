4a06dd3e33b5101b1efe897f4a32640642a0bab0
Hejx <531601727@qq.com>
2016-08-28 21:30:10 +0800

    base

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # vue-drag-and-drop
-A Vue.js directives with the HTML5 drag &amp; drop API. 
+A Vue.js directives with the HTML5 drag &amp; drop API.
+
+
+// DataTransfer 对象：退拽对象用来传递的媒介，使用一般为Event.dataTransfer。
+// draggable 属性：就是标签元素要设置draggable=true，否则不会有效果，例如：
+// <div title="拖拽我" draggable="true">列表1</div>
+// ondragstart 事件：当拖拽元素开始被拖拽的时候触发的事件，此事件作用在被拖曳元素上
+// ondragenter 事件：当拖曳元素进入目标元素的时候触发的事件，此事件作用在目标元素上
+// ondragover 事件：拖拽元素在目标元素上移动的时候触发的事件，此事件作用在目标元素上
+// ondrop 事件：被拖拽的元素在目标元素上同时鼠标放开触发的事件，此事件作用在目标元素上
+// ondragend 事件：当拖拽完成后触发的事件，此事件作用在被拖曳元素上
+// Event.preventDefault() 方法：阻止默认的些事件方法等执行。在ondragover中一定要执行preventDefault()，否则ondrop事件不会被触发。另外，如果是从其他应用软件或是文件中拖东西进来，尤其是图片的时候，默认的动作是显示这个图片或是相关信息，并不是真的执行drop。此时需要用用document的ondragover事件把它直接干掉。
+// Event.effectAllowed 属性：就是拖拽的效果。
---
+++ vue-drag-and-drop.js
@@ -0,0 +1,145 @@
+(function(){
+  var DragAndDrop = {};
+
+  DragAndDrop.install = function(Vue) {
+    Vue.directive('drag-and-drop', {
+      params: [
+        'dnd-drap',
+        'dnd-dragstart',
+        'dnd-dragend',
+        'dnd-dragover',
+        'dnd-dragenter',
+        'dnd-dragleave',
+        'dnd-drop',
+        'dnd-effect-allowed',
+        'dnd-allowed-types',
+        'dnd-type',
+        'dnd-inserted',
+        'dnd-selected',
+        'dnd-draggable'],
+      bind: function () {
+
+        // drag
+        this.handleDrag = function(event){
+          if (typeof(this.vm[this.params.dndDrag]) === 'function') {
+            this.vm[this.params.drag].call(this, event.target);
+          }
+        }.bind(this);
+
+        // dragstart
+        this.handleDragstart = function(event){
+          event = event.originalEvent || event;
+
+          var dndDraggable = this.vm[this.params.dndDraggable];
+          console.log(dndDraggable);
+          // Check whether the element is draggable, since dragstart might be triggered on a child.
+          if (dndDraggable == 'false') return true;
+
+          // Serialize the data associated with this element. IE only supports the Text drag type
+          event.dataTransfer.setData("Text", dndDraggable);
+
+          // Only allow actions specified in dnd-effect-allowed attribute
+          event.dataTransfer.effectAllowed = this.vm[this.params.dndEffectAllowed] || "move";
+
+          // Add CSS classes. See documentation above
+          this.el.classList.add("dndDragging");
+
+          // Try setting a proper drag image if triggered on a dnd-handle (won't work in IE).
+          if (event._dndHandle && event.dataTransfer.setDragImage) {
+            event.dataTransfer.setDragImage(this.el, 0, 0);
+          }
+
+          // Invoke callback
+          if (typeof(this.vm[this.params.dndDragstart]) === 'function') {
+            this.vm[this.params.dndDragstart].call(this, event.target);
+          }
+
+          event.stopPropagation();
+        }.bind(this);
+
+        // dragend
+        this.handleDragend = function(event){
+
+        }.bind(this);
+
+        // dragover
+        this.handleDragover = function(event){
+          event = event.originalEvent || event;
+
+          // todo: placeholder element
+
+          event.target.classList.add("dndDragover");
+          event.preventDefault();
+          event.stopPropagation();
+          return false;
+        }.bind(this);
+
+        // dragenter
+        this.handleDragenter = function(event){
+
+        }.bind(this);
+
+        // dragleave
+        this.handleDragleave = function(event){
+          event = event.originalEvent || event;
+          event.target.classList.remove("dndDragover");
+        }.bind(this);
+
+        // drop
+        this.handleDrop = function(event){
+          event = event.originalEvent || event;
+          event.target.classList.remove("dndDragover");
+          // Invoke callback
+          if (typeof(this.vm[this.params.dndDrop]) === 'function') {
+            this.vm[this.params.dndDrop].call(this, event.target);
+          }
+        }.bind(this);
+
+        // selected
+        var dndSelected = this.vm[this.params.dndSelected];
+        this.handleSelected = function(event){
+          // bind click event
+          if (typeof(dndSelected) === 'function') {
+            dndSelected.call(null, event.target);
+          }
+        }
+
+        this.el.setAttribute('draggable', true);
+        this.el.addEventListener('drag', this.handleDrag, false);
+        this.el.addEventListener('dragstart', this.handleDragstart, false);
+        this.el.addEventListener('dragend', this.handleDragend, false);
+        this.el.addEventListener('dragover', this.handleDragover, false);
+        this.el.addEventListener('dragenter', this.handleDragenter, false);
+        this.el.addEventListener('dragleave', this.handleDragleave, false);
+        this.el.addEventListener('drop', this.handleDrop, false);
+
+        this.el.addEventListener('click', this.handleSelected, false);
+      },
+      update: function (newValue, oldValue) {
+        // 值更新时的工作
+        // 也会以初始值为参数调用一次
+      },
+      unbind: function () {
+        // removeEventListener
+        this.el.removeEventListener('drag', this.handleDrag, false);
+        this.el.removeEventListener('dragstart', this.handleDragstart, false);
+        this.el.removeEventListener('dragend', this.handleDragend, false);
+        this.el.removeEventListener('dragover', this.handleDragover, false);
+        this.el.removeEventListener('dragenter', this.handleDragenter, false);
+        this.el.removeEventListener('dragleave', this.handleDragleave, false);
+        this.el.removeEventListener('drop', this.handleDrop, false);
+
+        this.el.removeEventListener('click', this.handleSelected, false);
+      }
+    })
+  }
+
+  if (typeof exports == "object") {
+    module.exports = DragAndDrop
+  } else if (typeof define == "function" && define.amd) {
+    define([], function(){ return DragAndDrop })
+  } else if (window.Vue) {
+    window.DragAndDrop = DragAndDrop
+    Vue.use(DragAndDrop)
+  }
+})();
