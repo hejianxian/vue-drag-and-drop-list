6f30c56ad1c65090836d0aab0485349995a738f0
Hejx <531601727@qq.com>
2016-09-02 00:19:11 +0800

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+let pkg = require('./package.json');
+
+let banner =
+  '/*!\n' +
+  ' * Vue-drap-and-drop-list.js v' + pkg.version + '\n' +
+  ' * (c) ' + new Date().getFullYear() + ' Hejx\n' +
+  ' * Released under the MIT License.\n' +
+	' * ' + pkg.homepage + '\n' +
+  ' */\n';
+
+export default {
+  entry: 'index.js',
+  banner: banner,
+  targets: [
+    {
+      dest: pkg['main'],
+      format: 'umd',
+      moduleName: 'DragAndDropList'
+    }
+  ]
+};
